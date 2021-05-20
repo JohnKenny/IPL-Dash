23c0f485f60784028ef801662ca33f7ced0bea38
JohnKenny <squareindigojohn@yahoo.ie>
2021-05-20 17:20:39 +0100

    default method added to MatchRepository

--- src/main/java/com/data/example/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/data/example/ipldashboard/repository/MatchRepository.java
@@ -3,11 +3,16 @@ package com.data.example.ipldashboard.repository;
 import com.data.example.ipldashboard.model.Match;
 import java.util.*;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+
+    default List<Match> findLatestMatchesByTeam(String teamName, int count){
+        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
+    }
     
 }
