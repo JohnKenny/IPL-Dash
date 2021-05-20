5f3d3776a9a8dcb5d6700eb6a614b8b97b1ecdf9
JohnKenny <squareindigojohn@yahoo.ie>
2021-05-20 17:09:23 +0100

    Sorted by date descending and added pageable

--- src/main/java/com/data/example/ipldashboard/controller/TeamController.java
+++ src/main/java/com/data/example/ipldashboard/controller/TeamController.java
@@ -4,6 +4,8 @@ import com.data.example.ipldashboard.model.Team;
 import com.data.example.ipldashboard.repository.MatchRepository;
 import com.data.example.ipldashboard.repository.TeamRepository;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,8 @@ public class TeamController {
     public Team getTeam(@PathVariable String teamName){
         
        Team team =  this.teamRepository.findByTeamName(teamName);
-       team.setMatches(matchRepository.getByTeam1OrTeam2(teamName, teamName)) ;
+       Pageable pageable = PageRequest.of(0, 4);
+       team.setMatches(matchRepository.getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, pageable)) ;
 
        return team; 
        
--- src/main/java/com/data/example/ipldashboard/repository/MatchRepository.java
+++ src/main/java/com/data/example/ipldashboard/repository/MatchRepository.java
@@ -2,10 +2,12 @@ package com.data.example.ipldashboard.repository;
 
 import com.data.example.ipldashboard.model.Match;
 import java.util.*;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
 
-    List<Match> getByTeam1OrTeam2(String teamName1, String teamName2);
+    List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
     
 }
