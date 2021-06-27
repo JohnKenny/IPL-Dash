5e8b908d862ca3a4fe1f2169ff4139792b585345
JohnKenny <squareindigojohn@yahoo.ie>
2021-06-27 15:24:07 +0100

    added routing and links

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,4 +1,5 @@
 import { React, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
@@ -6,12 +7,12 @@ export const TeamPage = () => {
 
     // team state intitialises with an empty array '[]'
     const [ team, setTeam ] = useState({matches: []});
-    
+    const { teamName } = useParams();
 
     useEffect(
         () => {
             const fetchMatches = async () => {
-                const response = await fetch('http://localhost:8080/team/Rajasthan Royals');
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
                  
@@ -19,14 +20,19 @@ export const TeamPage = () => {
             fetchMatches();
            
         },
-        [] // empty [] -> call useEffect only on first component load
+        // added dependency  
+        [teamName] // empty [] -> call useEffect only on first component load
     );
+
+  if(!team || !team.teamName){
+      return <h2>Team not found</h2>
+  }
 
   return (
     <div className="TeamPage">
         <h1>{ team.teamName }</h1> 
-        <MatchDetailCard match={team.matches[0]}/>
-        {team.matches.slice(1).map(match => <MatchSmallCard match={match}/>)}
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+        {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName}  match={match}/>)}
            
     </div>
   );
