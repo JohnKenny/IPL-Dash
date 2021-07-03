9021af8b72cffced00bf3f5e6329772bc7b5fb32
JohnKenny <squareindigojohn@yahoo.ie>
2021-07-03 17:16:38 +0100

    added styling 2

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -10,14 +10,23 @@ export const MatchDetailCard = ({teamName, match}) => {
   return (
     <div className="MatchDetailCard">
         <span className="vs">vs</span>
-        <h2><Link to={otherTeamRoute}>{otherTeam}</Link></h2>
-        <h3 className="match-date">{match.date}</h3>
-        <h4 className="match-venue">{match.venue}</h4>
-        <h4 className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</h4>
+        <div className="additional-detail">
         <h4>First Innings</h4>
         <p>{match.team1}</p>
         <h4>Second Innings</h4>
         <p>{match.team2}</p>
+        <h4>Man of the match</h4>
+        <p>{match.playerOfMatch}</p>
+        <h4>Umpires</h4>
+        <p>{match.umpire1}, {match.umpire2}</p>
+        </div>
+        <div>
+        <h2><Link to={otherTeamRoute}>{otherTeam}</Link></h2>
+        <h3 className="match-date">{match.date}</h3>
+        <h4 className="match-venue">{match.venue}</h4>
+        <h4 className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</h4>
+        </div>
+        
     </div>
   );
 }
