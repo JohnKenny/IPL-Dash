7ae93d906dcd19d712441b7d3b780c57cc17eecd
JohnKenny <squareindigojohn@yahoo.ie>
2021-07-05 15:03:23 +0100

    added chart to team page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
+import { PieChart } from 'react-minimal-pie-chart';
+
+
 import './TeamPage.scss';
 
 export const TeamPage = () => {
@@ -33,14 +36,21 @@ export const TeamPage = () => {
   return (
     <div className="TeamPage">
         <div className="team-name-section"><h1 className="team-name">{ team.teamName }</h1></div> 
-        <div className="win-loss-section">Wins / Losses</div>
+        <div className="win-loss-section">Wins / Losses
+        <PieChart
+            data={[
+            { title: 'Losses', value: team.totalMatches - team.totalWins, color: 'crimson' },
+            { title: 'Wins', value: team.totalWins, color: 'rgb(197, 89, 197)' }          
+            ]}
+            />
+        </div>
         <div className="match-detail-section">
             <h3>Latest matches</h3>    
             <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
         </div>
         {team.matches.slice(1).map(match => <MatchSmallCard teamName={team.teamName}  match={match}/>)}
         <div className="more-link">
-            <a href="#">More ></a>
+            <a href="#">More {'>'} </a>
         </div>
     </div>
   );
